ab1b49e4bd6da787d0329abe010669de3c9f3fc8
Artur Malabarba <bruce.connor.am@gmail.com>
2013-08-14 19:20:53 +0100

    Imported code.

--- lisp-assistant.el
+++ lisp-assistant.el
@@ -7,7 +7,7 @@
 ;; Version: 0.1
 ;; Keywords: lisp tools
 ;; ShortName: lisa
-;; Separator: /
+;; Separator: -
 
 ;;; Commentary:
 ;; 
@@ -36,34 +36,37 @@
 ;; 
 
 ;;; Change Log:
+;; 0.1 - 20130814 - Imported code.
 ;; 0.1 - 20130813 - Created File.
 ;;; Code:
 
-(defconst lisa/version "0.1" "Version of the lisp-assistant.el package.")
-(defconst lisa/version-int 1 "Version of the lisp-assistant.el package, as an integer.")
-(defun lisa/bug-report ()
+(defconst lisa-version "0.1" "Version of the lisp-assistant.el package.")
+(defconst lisa-version-int 1 "Version of the lisp-assistant.el package, as an integer.")
+(defun lisa-bug-report ()
   "Opens github issues page in a web browser. Please send me any bugs you find, and please inclue your emacs and lisa versions."
   (interactive)
   (browse-url "https://github.com/Bruce-Connor/lisp-assistant/issues/new")
-  (message "Your lisa/version is: %s, and your emacs version is: %s.\nPlease include this in your report!"
-           lisa/version emacs-version))
-(defun lisa/customize ()
+  (message "Your lisa-version is: %s, and your emacs version is: %s.\nPlease include this in your report!"
+           lisa-version emacs-version))
+(defun lisa-customize ()
   "Open the customization menu in the `lisp-assistant' group."
   (interactive)
   (customize-group 'lisp-assistant t))
 
 ;;; ---------------------------------------------------------------------
 ;;; Package handling variables
-(defvar lsp/package-name       nil "")
-(defvar lsp/short-package-name nil "")
-(defvar lsp/package-version    nil "")
-(defvar lsp/package-change-log "" "")
-(defcustom lsp/separator "/"
-  "Separator to use after a `lsp/short-package-name'."
+(defvar lisa-package-name       nil "")
+(defvar lisa-short-package-name nil "")
+(defvar lisa-package-version    nil "")
+(defvar lisa-package-change-log "" "")
+(defcustom lisa-separator "-"
+  "Separator to use after a `lisa-short-package-name'.
+This is usually a \"-\", but some people like to use \"/\" or
+\":\"."
   :type 'string
-  :group 'lisp
-  :package-version '(lisp . "0.1"))
-(defcustom lsp/github-username nil
+  :group 'lisp-assistant
+  :package-version '(lisp-assistant . "0.1"))
+(defcustom lisa-github-username nil
   "Username used to construct github urls inside templates.
 
 Must be exactly as it appears in your github urls. For instance, here
@@ -72,104 +75,147 @@ the username would be \"Bruce-Connor\".
 
 Leave this nil if you don't want to use it."
   :type 'string
-  :group 'lisp
-  :package-version '(lisp . "0.1"))
-(make-variable-buffer-local 'lsp/package-name)
-(make-variable-buffer-local 'lsp/short-package-name)
-(make-variable-buffer-local 'lsp/package-version)
-(make-variable-buffer-local 'lsp/package-change-log)
-(make-variable-buffer-local 'lsp/separator)
-(defcustom lsp/default-version-number "0.1"
+  :group 'lisp-assistant
+  :package-version '(lisp-assistant . "0.1"))
+(make-variable-buffer-local 'lisa-package-name)
+(make-variable-buffer-local 'lisa-short-package-name)
+(make-variable-buffer-local 'lisa-package-version)
+(make-variable-buffer-local 'lisa-package-change-log)
+(make-variable-buffer-local 'lisa-separator)
+(defcustom lisa-default-version-number "0.1"
   "Version number for newly created packages."
   :type 'string
-  :group 'lisp
-  :package-version '(lisp . "0.1"))
+  :group 'lisp-assistant
+  :package-version '(lisp-assistant . "0.1"))
 
 ;;; ---------------------------------------------------------------------
 ;;; Package handling functions
-(defun lsp/insert-change-log (changeVersion &optional log)
+(defun lisa-insert-change-log (changeVersion &optional log)
   "Insert a commented line with date and version."
   (interactive "P")
   (let ((wasChanged (buffer-modified-p)))
-    (when changeVersion (lsp/update-version-number))
+    (when changeVersion (lisa-update-version-number))
     (if (called-interactively-p 'interactive)
         (setq log (read-string "Change Log: ")))
     (goto-char (point-min))
     (search-forward-regexp ";;; Change Log: *\n")
     (insert "\n")
     (forward-char -1)
-    (insert ";; " lsp/package-version " - "
+    (insert ";; " lisa-package-version " - "
             (format-time-string "%Y%m%d") 
             " - ")
     (save-excursion
       (insert log "."))
-    (setq lsp/package-change-log
-          (concat lsp/package-change-log "\n" log "."))
+    (setq lisa-package-change-log
+          (concat lisa-package-change-log "\n" log "."))
     (unless wasChanged (save-buffer))))
 
-(defun lsp/update-version-number ()
+(defun lisa-update-version-number ()
   "Change the version number of current package."
   (interactive)
-  (let ((vo lsp/package-version))
-    (setq lsp/package-version (read-string "New Version Number: " vo))
+  (let ((vo lisa-package-version))
+    (setq lisa-package-version (read-string "New Version Number: " vo))
     (goto-char (point-min))
     (if (search-forward (concat ";; Version: " vo) nil t)
-        (replace-match (concat ";; Version: " lsp/package-version))
-      (setq lsp/package-version vo)
-      (error "Couldn't find ';; Version:' comment. `lsp/package-version' variable not updated."))
+        (replace-match (concat ";; Version: " lisa-package-version))
+      (setq lisa-package-version vo)
+      (error "Couldn't find ';; Version:' comment. `lisa-package-version' variable not updated."))
     (if (search-forward-regexp
-         (concat "(defconst *" lsp/short-package-name lsp/separator "version *\"\\(" vo "\\)")
+         (concat "(defconst *" lisa-short-package-name lisa-separator "version *\"\\(" vo "\\)")
          nil t)
-        (replace-match lsp/package-version nil nil nil 1)
+        (replace-match lisa-package-version nil nil nil 1)
       (warn "Couldn't find version 'defconst'."))
     (if (search-forward-regexp
-         (concat "(defconst *" lsp/short-package-name lsp/separator "version-int *\\([0-9]+\\)")
+         (concat "(defconst *" lisa-short-package-name lisa-separator "version-int *\\([0-9]+\\)")
          nil t)
         (replace-match (int-to-string (1+ (string-to-number (match-string 1))))
                        nil nil nil 1))))
 
-(defun lsp/define-package-variables ()
+(defun lisa-define-package-variables ()
   "Guess package-specific variables from file name and content."
   (interactive)
   (save-excursion
     (goto-char (point-min))
-    (setq lsp/package-name (if (search-forward-regexp "(provide '\\([^)]+\\))" nil t)
+    (setq lisa-package-name (if (search-forward-regexp "(provide '\\([^)]+\\))" nil t)
                                (match-string-no-properties 1)
                              (file-name-base (or (buffer-file-name)
                                                  (buffer-name) ""))))
     (goto-char (point-min))
-    (setq lsp/short-package-name
+    (setq lisa-short-package-name
           (if (search-forward-regexp ";;+ +ShortName: +\\([-/:a-zA-Z0-9\.]+\\)" nil t)
               (match-string-no-properties 1)
             (let ((cand (replace-regexp-in-string
                          "^\\([a-z]\\)[a-z]*\\-\\([a-z]\\)[a-z]*\\-\\([a-z]\\)[a-z]*"
-                         "\\1\\2\\3" lsp/package-name)))
-              (if (string= lsp/package-name cand) nil cand))))
+                         "\\1\\2\\3" lisa-package-name)))
+              (if (string= lisa-package-name cand) nil cand))))
     (goto-char (point-min))
-    (setq lsp/package-version
+    (setq lisa-package-version
           (if (search-forward-regexp ";;+ +Version: +\\([0-9a-zA-Z\.]+\\)" nil t)
               (match-string-no-properties 1) nil))
     (goto-char (point-min))
     (if (search-forward-regexp ";;+ +Separator: +\\([^ \n',`(){}]+\\)" nil t)
-        (setq lsp/separator (match-string-no-properties 1)))))
+        (setq lisa-separator (match-string-no-properties 1)))))
 
 
 ;;; ---------------------------------------------------------------------
-;;; Lisp Keymap
-(define-prefix-command 'my-eval-lisp-map)
-(define-key emacs-lisp-mode-map "e"      'my-eval-lisp-map)
+;;; General coding functions
+(defun lisa-find-or-define-function ()
+  "Like `find-function' unless function is not defined, then create a defun with it."
+  (interactive)
+  (if (function-called-at-point)
+      (find-function (function-called-at-point))
+    (push-mark)
+    (let ((name (thing-at-point 'symbol)))
+      (end-of-defun)
+      (insert "\n(df)\n")
+      (backward-char 2)
+      (yas-expand)
+      (insert name))))
 
-(define-key my-eval-lisp-map "b" 'eval-buffer)
-(define-key my-eval-lisp-map "d" 'eval-defun)
-(define-key my-eval-lisp-map "e" 'eval-expression)
-;; (define-key my-eval-lisp-map "l" 'eval-last-sexp)
-(define-key my-eval-lisp-map "p" 'eval-print-last-sexp)
-(define-key my-eval-lisp-map "r" 'eval-region)
-(define-key my-eval-lisp-map "n"      'lsp/define-package-variables)
-(define-key my-eval-lisp-map "#"      'lsp/define-package-variables)
-(define-key my-eval-lisp-map "l"      'lsp/insert-change-log)
-(define-key my-eval-lisp-map "f"     'lsp/find-or-define-function)
-(define-key my-eval-lisp-map "v"     'lsp/find-or-define-variable)
+(defun lisa-find-or-define-variable ()
+  "Like `find-variable' unless variable is not defined, then create a defcustom with it."
+  (interactive)
+  (if (symbolp (variable-at-point))
+      (find-variable (variable-at-point))
+    (push-mark)
+    (let ((name (thing-at-point 'symbol)))
+      (beginning-of-defun)
+      (insert "(dc)\n\n")
+      (backward-char 3)
+      (yas-expand)
+      (insert name))))
+
+(defun lisa-comment-sexp ()
+  "Comment current sexp."
+  (interactive)
+  (if (looking-at ";")
+      (progn
+        (er/expand-region 1)
+        (uncomment-region (region-beginning) (region-end)))
+    (unless (looking-at "(")
+      (condition-case nil
+          (paredit-backward-up)
+        (error (forward-sexp -1))))
+    (mark-sexp)
+    (comment-region (region-beginning) (region-end))))
+
+
+;;; ---------------------------------------------------------------------
+;;; Lisp-Assistant Keymap
+(define-prefix-command 'lisa-map)
+(define-key lisp-assistant-mode-map "e"      'lisa-map)
+
+(define-key lisa-map "b" 'eval-buffer)
+(define-key lisa-map "d" 'eval-defun)
+(define-key lisa-map "e" 'eval-expression)
+;; (define-key lisa-map "l" 'eval-last-sexp)
+(define-key lisa-map "p" 'eval-print-last-sexp)
+(define-key lisa-map "r" 'eval-region)
+(define-key lisa-map "n"      'lisa-define-package-variables)
+(define-key lisa-map "#"      'lisa-define-package-variables)
+(define-key lisa-map "l"      'lisa-insert-change-log)
+(define-key lisa-map "f"     'lisa-find-or-define-function)
+(define-key lisa-map "v"     'lisa-find-or-define-variable)
 
 ;;;###autoload
 
